0ce66fe42176900142626e4362fcbd493bb192a4
Sam Willis <sam@williscloud.org>
2023-04-13 21:35:04 +1000

    Updated dashboard.

--- config/lua/theme.lua
+++ config/lua/theme.lua
@@ -25,7 +25,6 @@ require("dashboard").setup({
 			enable = false,
 		},
 		shortcut = {
-			{ desc = " Update", group = "@property", action = "Lazy update", key = "u" },
 			{
 				icon = " ",
 				icon_hl = "@variable",
@@ -33,18 +32,6 @@ require("dashboard").setup({
 				group = "Label",
 				action = "Telescope find_files",
 				key = "f",
-			},
-			{
-				desc = " Apps",
-				group = "DiagnosticHint",
-				action = "Telescope app",
-				key = "a",
-			},
-			{
-				desc = " dotfiles",
-				group = "Number",
-				action = "Telescope dotfiles",
-				key = "d",
 			},
 		},
 	},
